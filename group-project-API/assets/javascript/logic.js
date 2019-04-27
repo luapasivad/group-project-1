d1c90b3a71a6c7773b9cba58bec58c505650ce26
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-04-27 11:15:35 -0400

    added filter row with functional buttons

--- group-project-API/assets/javascript/logic.js
+++ group-project-API/assets/javascript/logic.js
@@ -1,89 +1,115 @@
 var locationTxt = $('#locationTxt'),
     searchTxt = $('#searchTxt'),
-    searchBtn = $('#searchBtn'),
+    searchBtn = $('#searchBtn'), // btns and text
     resultsDiv = $('#results'),
-    results_array = [],
+    dropdownCatagories = $('#dropdown-catagories'),
+    dropdownSortBy = $('#dropdown-SortBy'),
+    priceBtn = $('#price')
+    results_array = [], // stored array for results (not necessary)
     latSearch = 0,
-    lngSearch = 0;
+    lngSearch = 0, // starting lal-lng value for map
+    cats = [],
+    city = "", // global city term
+    searchTerm = "", // global search term
+    howMuch = 2, // price variable starting at 2
+    sorting = "best_match";
 
 
 
 searchBtn.on('click', function() {
+    searchTerm = searchTxt.val(); // text box
+    city = locationTxt.val(); // text box
+    search(city, searchTerm, howMuch, sorting) // function    
+})
 
-    let city = locationTxt.val(),
-        searchTerm = searchTxt.val();
+dropdownCatagories.on('click', 'a', function(){
+    // console.log($(this).attr('data-search')) // test
+    searchTerm = $(this).attr('data-search') // data from clicking dropdown
+    search(city, searchTerm, howMuch, sorting) // function
 
-    resultsDiv.empty()
+})
 
-    $.ajax({
-        url: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=${city}`,
+priceBtn.on('click', '.btn', function(){
+    // console.log($(this).attr('data-howMuch')) // test
+    howMuch = parseInt($(this).attr('data-howMuch')) // data from clicking button and change to number
+    search(city, searchTerm, howMuch, sorting) // function
+})
+
+dropdownSortBy.on('click', 'a', function() {
+    // console.log($(this).attr('data-sortBy')) // test
+    sorting = $(this).attr('data-sortBy') // data from clicking button
+    search(city, searchTerm, howMuch, sorting) // search
+})
+
+function search(where, what, price, sort) {
+    resultsDiv.empty() // clears results
+    cats = [] // clears catagories for search
+
+    var queryurl = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${what}&location=${where}&price=${price}&sort_by=${sort}`
+    // dynamic URL ^^
+
+    $.ajax({ // call
+        url: queryurl,
         headers: {
             'Authorization':'Bearer ByKJz0Ky8pL1uwXzQSTxvANlim2VHt4zmsFriFSgK-rXw_I6zpeQrS9EbF_2Qh8fRJ5BqUG6fqK5R-GoGF0bGgClK-wSPgJfUeEY8_bIPTXT5EtUpgnZo0Pvp4zDXHYx'
         },
         method: "GET",
         }).then(function(response) {
-            console.log(response)
-            searchTxt.val("")
-            locationTxt.val("")
+            // console.log(response) // test
+            dropdownCatagories.empty() // empty catagories dropdown to repopulate
+            let businesses = response.businesses;
 
-            latSearch = response.businesses[0].coordinates.latitude
-            lngSearch = response.businesses[0].coordinates.longitude
+            searchTxt.val("") // delete text in search boxes
+            // locationTxt.val("") 
 
-
-            initMap()
-
-            for (let i=0; i < response.businesses.length; i++){
-                results_array.push(response.businesses[i])
+            latSearch = businesses[0].coordinates.latitude
+            lngSearch = businesses[0].coordinates.longitude // coordinates for first location listed
 
 
-                $('<div>')   
+            initMap() // call map functionm
+
+            for (let i=0; i < businesses.length; i++){
+                results_array.push(businesses[i]) // adds to result array
+                console.log(cats) //catagories test
+
+                $('<div>') // container for each result
                     .attr('class', 'resultsContainer m-2 w-100')      
-                    .attr('id', response.businesses[i].id) 
+                    .attr('id', businesses[i].id) 
                     .appendTo(resultsDiv)      
-                $('<img>')
-                    .attr('style', 'height: 200px; width: 200px; display: inline-block;')
-
-                    .attr('src', response.businesses[i].image_url)
-                    .appendTo('#' + response.businesses[i].id)
-                $('<span>')
+                $('<img>') // image from result
+                    .attr('style', 'height: 100px; width: 100px; display: inline-block;')
+                    .attr('class', 'img-fluid')
+                    .attr('src', businesses[i].image_url)
+                    .appendTo('#' + businesses[i].id)
+                $('<span>') // title
                     .attr('class', 'resultsText ml-2')
-                    .text(response.businesses[i].name)
-                    .appendTo('#' + response.businesses[i].id)
+                    .text(businesses[i].name)
+                    .appendTo('#' + businesses[i].id)
 
-                    
-                
-            }
-            console.log(results_array)
-        });
+                let categories = businesses[i].categories
         
-})
+                for (let j = 0; j < categories.length; j++) { // pull categories from search
+                    let title = categories[j].title;
+                    if (cats.indexOf(title) === -1 && cats.length < 7) {
+                        $('<a>')
+                            .attr('class', 'dropdown-item')
+                            .text(title)
+                            .attr('data-search', title)
+                            .appendTo(dropdownCatagories) // create in dropdown
+                    
+                        cats.push(title) // array of categories (not sure if needed)
+                    }
+                }
+            }
+            console.log(results_array) // for test
+        });
+    }
 
-
-
-function initMap() {
+function initMap() { // map
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 8,
       center: {lat: latSearch, lng: lngSearch}
     });
-    var geocoder = new google.maps.Geocoder();
-  
-    // document.getElementById('submit').addEventListener('click', function() {
-    //   geocodeAddress(geocoder, map);
-    // });
+
   
   }
-  
-//   function geocodeAddress(geocoder, resultsMap) {
-//     var address = document.getElementById('address').value;
-//     geocoder.geocode({'address': address}, function(results, status) {
-//       if (status === 'OK') {
-//         resultsMap.setCenter(results[0].geometry.location);
-//         var marker = new google.maps.Marker({
-//           map: resultsMap,
-//           position: results[0].geometry.location
-//         });
-//       } else {
-//         alert('Geocode was not successful for the following reason: ' + status);
-//       }
-//     });
-//   }
